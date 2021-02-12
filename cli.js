554c3ffa5222e76b5b1069827aff6dab64e1b49b
CHAHAR <39922821+CHAHAR117@users.noreply.github.com>
2021-02-12 10:28:45 +0530

    bug fix

--- cli.js
+++ cli.js
@@ -3,7 +3,6 @@
 const operating_system = require('./os_details');
 const numCPU = require('./cpu_details').number_of_cpus;
 const detailCPU = require('./cpu_details').details_of_cpus;
-const { spawn } = require("child_process");
 const chalk = require('chalk');
 console.log("\n"+chalk.cyan.underline.bold("Operating System Details :"));
 console.table(operating_system);
@@ -11,15 +10,7 @@ console.log("\n"+chalk.cyan.underline.bold("CPUs Details :"));
 console.table(numCPU);
 console.table(detailCPU);
 console.log("\n"+chalk.cyan.underline.bold("Network Speed :")+" (will take 1-2 minutes to calculate)");
-const child = spawn('npm run speedtest-net', {
-    stdio: 'inherit',
-    shell: true
-    }).on('data', (chunk) => {
-    console.log(chunk);
-    });
-
-
-
+require('./network_speed');
 
 
 
